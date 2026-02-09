e1c2ea96f23481262c98692322fb3a5fa48c3171
Ihlon Gyve Nogot <135338493+MeekSharkdev@users.noreply.github.com>
2026-02-09 15:17:11 +0800

    Update mysqli_connect_req.php

--- config/mysqli_connect_req.php
+++ config/mysqli_connect_req.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost";  // Your server name
-$username = "root";         // Your database username
-$password = "122401";             // Your database password (usually empty for localhost)
-$dbname = "dbusers";    // Your database name
+$servername = "localhost";  
+$username = "root";         
+$password = "122401";             
+$dbname = "dbusers";   
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check the connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);  // Exit if connection fails
+    die("Connection failed: " . $conn->connect_error);  
 }
 ?>
